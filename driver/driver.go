bf9a6b72120e8d0b86a54f787331c62bd763415f
Scott Weiss <scott.weiss@emc.com>
2015-12-23 19:33:36 -0500

    add mutex to prevent race

--- driver/driver.go
+++ driver/driver.go
@@ -1,14 +1,17 @@
 package driver
 import (
 	"github.com/layer-x/layerx-commons/lxactionqueue"
+	"sync"
 )
 
 type tpiDriver struct {
+	lock sync.Mutex
 	actionQueue lxactionqueue.ActionQueue
 }
 
 func NewMesosTpiDriver(actionQueue lxactionqueue.ActionQueue) *tpiDriver {
 	return &tpiDriver{
+		lock: sync.Mutex{},
 		actionQueue: actionQueue,
 	}
 }
@@ -16,6 +19,8 @@ func NewMesosTpiDriver(actionQueue lxactionqueue.ActionQueue) *tpiDriver {
 //run as goroutine
 func (d *tpiDriver) Run() {
 	for {
+		d.lock.Lock()
 		d.actionQueue.ExecuteNext()
+		d.lock.Unlock()
 	}
 }
