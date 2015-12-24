cbb8b405fb8a939789e1b6fbfdb90363df02fc81
Scott Weiss <scott.weiss@emc.com>
2015-12-24 12:37:59 -0500

    layerx tpi client (for use by TPIs)

---
+++ layerx_tpi/layerx_tpi.go
@@ -0,0 +1,135 @@
+package layerx_tpi
+import (
+	"github.com/layer-x/layerx-commons/lxerrors"
+	"github.com/layer-x/layerx-core_v2/lxtypes"
+	"github.com/mesos/mesos-go/mesosproto"
+	"github.com/layer-x/layerx-commons/lxhttpclient"
+	"fmt"
+	"encoding/json"
+)
+
+type LayerXTpi struct {
+	CoreURL		string
+}
+
+const (
+	RegisterTaskProvider = "/RegisterTaskProvider"
+	DeregisterTaskProvider   = "/DeregisterTaskProvider"
+	GetTaskProviders   = "/GetTaskProviders"
+	GetStatusUpdates   = "/GetStatusUpdates"
+	SubmitTask   = "/SubmitTask"
+	KillTask   = "/KillTask"
+	PurgeTask   = "/PurgeTask"
+)
+
+//call this method when registering
+// a new task provider to the tpi
+func (tpi *LayerXTpi) RegisterTaskProvider(tp *lxtypes.TaskProvider) error {
+	resp, _, err := lxhttpclient.Post(tpi.CoreURL, RegisterTaskProvider, nil, tp)
+	if err != nil {
+		return lxerrors.New("POSTing TaskProvider to LayerX core server", err)
+	}
+	if resp.StatusCode != 202 {
+		msg := fmt.Sprintf("POSTing TaskProvider to LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return lxerrors.New(msg, err)
+	}
+	return nil
+}
+
+//call this method when a non-failover task provider
+// disconnects from the tpi
+func (tpi *LayerXTpi) DeregisterTaskProvider(tpId string) error {
+	resp, _, err := lxhttpclient.Post(tpi.CoreURL, DeregisterTaskProvider+"/"+tpId, nil, nil)
+	if err != nil {
+		return lxerrors.New("Requesting DeRegister of task provider "+tpId+" to LayerX core server", err)
+	}
+	if resp.StatusCode != 202 {
+		msg := fmt.Sprintf("Requesting DeRegister of task provider "+tpId+" to LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return lxerrors.New(msg, err)
+	}
+	return nil
+}
+
+//call this method to retrieve a list of registered
+//task providers. e.g. before polling task providers
+//for pending tasks
+func (tpi *LayerXTpi) GetTaskProviders() ([]*lxtypes.TaskProvider, error) {
+	taskProviders := []*lxtypes.TaskProvider{}
+	resp, body, err := lxhttpclient.Get(tpi.CoreURL, GetTaskProviders, nil)
+	if err != nil {
+		return nil, lxerrors.New("Requesting task provider list from LayerX core server", err)
+	}
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Requesting task provider list from LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return nil, lxerrors.New(msg, err)
+	}
+	err = json.Unmarshal(body, &taskProviders)
+	if err != nil {
+		return nil, lxerrors.New("unmarshalling json to []*lxtypes.TaskProvider", err)
+	}
+	return taskProviders, nil
+
+}
+
+//call this method to retrieve a list of
+// the most recent status updates for tasks
+func (tpi *LayerXTpi) GetStatusUpdates() ([]*mesosproto.TaskStatus, error) {
+	statusUpdates := []*mesosproto.TaskStatus{}
+	resp, body, err := lxhttpclient.Get(tpi.CoreURL, GetStatusUpdates, nil)
+	if err != nil {
+		return nil, lxerrors.New("Requesting status update list from LayerX core server", err)
+	}
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Requesting status update list from LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return nil, lxerrors.New(msg, err)
+	}
+	err = json.Unmarshal(body, &statusUpdates)
+	if err != nil {
+		return nil, lxerrors.New("unmarshalling json to []*mesosproto.TaskStatus", err)
+	}
+	return statusUpdates, nil
+}
+
+//call this method to submit
+// a requested task to layer-x
+func (tpi *LayerXTpi) SubmitTask(task *lxtypes.Task) error {
+	resp, _, err := lxhttpclient.Post(tpi.CoreURL, SubmitTask, nil, task)
+	if err != nil {
+		return lxerrors.New("POSTing Task to LayerX core server", err)
+	}
+	if resp.StatusCode != 202 {
+		msg := fmt.Sprintf("POSTing Task to LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return lxerrors.New(msg, err)
+	}
+	return nil
+}
+
+//call this method to submit
+// a requested task to layer-x
+func (tpi *LayerXTpi) KillTask(taskId string) error {
+	resp, _, err := lxhttpclient.Post(tpi.CoreURL, KillTask+"/"+taskId, nil, nil)
+	if err != nil {
+		return lxerrors.New("Requesting KILL on task "+taskId+" to LayerX core server", err)
+	}
+	if resp.StatusCode != 202 {
+		msg := fmt.Sprintf("Requesting KILL on task "+taskId+" to LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return lxerrors.New(msg, err)
+	}
+	return nil
+}
+
+//call this method to purge
+// a requested task from layer-x
+func (tpi *LayerXTpi) PurgeTask(taskId string) error {
+
+	resp, _, err := lxhttpclient.Post(tpi.CoreURL, PurgeTask+"/"+taskId, nil, nil)
+	if err != nil {
+		return lxerrors.New("Requesting Purge on task "+taskId+" to LayerX core server", err)
+	}
+	if resp.StatusCode != 202 {
+		msg := fmt.Sprintf("Requesting Purge on task "+taskId+" to LayerX core server; status code was %v, expected 202", resp.StatusCode)
+		return lxerrors.New(msg, err)
+	}
+	return nil
+}
+
---
+++ layerx_tpi/layerx_tpi_test.go
@@ -0,0 +1,107 @@
+package layerx_tpi_test
+
+import (
+	. "github.com/layer-x/layerx-core_v2/layerx_tpi"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/layer-x/layerx-core_v2/fakes"
+	"github.com/layer-x/layerx-core_v2/lxtypes"
+	"github.com/mesos/mesos-go/mesosproto"
+)
+
+var _ = Describe("LayerxTpi", func() {
+
+	fakeStatus1 := fakes.FakeTaskStatus("fake_task_id_1", mesosproto.TaskState_TASK_RUNNING)
+	fakeStatus2 := fakes.FakeTaskStatus("fake_task_id_2", mesosproto.TaskState_TASK_KILLED)
+	fakeStatus3 := fakes.FakeTaskStatus("fake_task_id_3", mesosproto.TaskState_TASK_FINISHED)
+
+	fakeStatuses := []*mesosproto.TaskStatus{fakeStatus1, fakeStatus2, fakeStatus3}
+
+	go fakes.RunFakeLayerXServer(fakeStatuses, 12345)
+	lxTpi := LayerXTpi{
+		CoreURL: "127.0.0.1:12345",
+	}
+	Describe("RegisterTaskProvider", func(){
+		It("submits a new task provider to the LX Server", func(){
+			taskProvider := &lxtypes.TaskProvider{
+				Id: "fake_task_provder_id",
+				Source: "taskprovider@tphost:port",
+			}
+			err := lxTpi.RegisterTaskProvider(taskProvider)
+			Expect(err).To(BeNil())
+		})
+	})
+	Describe("DeregisterTaskProvider", func(){
+		It("Requests the server to delete the task provider", func(){
+			err := lxTpi.DeregisterTaskProvider("fake_task_provder_id")
+			Expect(err).To(BeNil())
+			err = lxTpi.DeregisterTaskProvider("fake_task_provder_id")
+			Expect(err).ToNot(BeNil())
+		})
+	})
+	Describe("GetTaskProviders", func(){
+		It("returns a list of registered task providers", func(){
+			taskProvider1 := &lxtypes.TaskProvider{
+				Id: "fake_task_provder_id_1",
+				Source: "taskprovider1@tphost:port",
+			}
+			err := lxTpi.RegisterTaskProvider(taskProvider1)
+			Expect(err).To(BeNil())
+
+			taskProvider2 := &lxtypes.TaskProvider{
+				Id: "fake_task_provder_id_2",
+				Source: "taskprovider2@tphost:port",
+			}
+			err = lxTpi.RegisterTaskProvider(taskProvider2)
+			Expect(err).To(BeNil())
+
+			taskProvider3 := &lxtypes.TaskProvider{
+				Id: "fake_task_provder_id_3",
+				Source: "taskprovider2@tphost:port",
+			}
+			err = lxTpi.RegisterTaskProvider(taskProvider3)
+			Expect(err).To(BeNil())
+
+			taskProviders, err := lxTpi.GetTaskProviders()
+			Expect(err).To(BeNil())
+			Expect(taskProviders).To(ContainElement(taskProvider1))
+			Expect(taskProviders).To(ContainElement(taskProvider2))
+			Expect(taskProviders).To(ContainElement(taskProvider3))
+		})
+	})
+	Describe("GetStatusUpdates", func(){
+		It("returns a list of status updates", func(){
+			statuses, err := lxTpi.GetStatusUpdates()
+			Expect(err).To(BeNil())
+			Expect(statuses).To(ContainElement(fakeStatus1))
+			Expect(statuses).To(ContainElement(fakeStatus2))
+			Expect(statuses).To(ContainElement(fakeStatus3))
+		})
+	})
+	Describe("SubmitTask", func(){
+		It("submits a task to the server", func(){
+			fakeLxTask := fakes.FakeLXTask("fake_task_id", "fake_task_name", "fake_slave_id", "echo FAKE_COMMAND")
+			err := lxTpi.SubmitTask(fakeLxTask)
+			Expect(err).To(BeNil())
+		})
+	})
+	Describe("KillTask", func(){
+		It("requests server to flag task with KillRequested", func(){
+			err := lxTpi.KillTask("fake_task_id")
+			Expect(err).To(BeNil())
+		})
+	})
+	Describe("PurgeTask", func(){
+		It("requests server to flag remove the task from its database", func(){
+			fakeLxTask := fakes.FakeLXTask("fake_task_id", "fake_task_name", "fake_slave_id", "echo FAKE_COMMAND")
+			err := lxTpi.SubmitTask(fakeLxTask)
+			Expect(err).To(BeNil())
+			err = lxTpi.PurgeTask("fake_task_id")
+			Expect(err).To(BeNil())
+			err = lxTpi.PurgeTask("fake_task_id")
+			Expect(err).ToNot(BeNil())
+		})
+	})
+
+})
