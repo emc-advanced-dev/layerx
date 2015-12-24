35900d343289e81fd695bd1fdfbad71cf121ce3d
Scott Weiss <scott.weiss@emc.com>
2015-12-23 19:35:00 -0500

    Add fake Proto data for tests

---
+++ fakes/fake_proto_objects.go
@@ -0,0 +1,36 @@
+package fakes
+import (
+	"github.com/gogo/protobuf/proto"
+	"github.com/mesos/mesos-go/mesosproto"
+)
+
+
+func FakeFramework() *mesosproto.FrameworkInfo {
+	capabilityType := mesosproto.FrameworkInfo_Capability_REVOCABLE_RESOURCES
+	return &mesosproto.FrameworkInfo{
+		User: proto.String("fake_framework_user"),
+		Name: proto.String("fake_framework"),
+		Id: &mesosproto.FrameworkID{
+			Value: proto.String("fake_framework_id"),
+		},
+		FailoverTimeout: proto.Float64(0),
+		Checkpoint:      proto.Bool(false),
+		Role:            proto.String("*"),
+		Hostname:        proto.String("fake_host"),
+		Principal:       proto.String("fake_principal"),
+		WebuiUrl:        proto.String("http://fakeip:fakeport"),
+		Capabilities: []*mesosproto.FrameworkInfo_Capability{
+			&mesosproto.FrameworkInfo_Capability{
+				Type: &capabilityType,
+			},
+		},
+		Labels: &mesosproto.Labels{
+			Labels: []*mesosproto.Label{
+				&mesosproto.Label{
+					Key:   proto.String("FakeLabel"),
+					Value: proto.String("FakeValue"),
+				},
+			},
+		},
+	}
+}
