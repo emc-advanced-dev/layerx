42576c35a8bb62687199079e75e8aac5ddc266b5
Scott Weiss <scott.weiss@emc.com>
2015-12-26 20:16:11 -0500

    message to pass to the tpi

---
+++ layerx_tpi/tpi_messages.go
@@ -0,0 +1,14 @@
+package layerx_tpi
+import (
+	"github.com/layer-x/layerx-core_v2/lxtypes"
+	"github.com/mesos/mesos-go/mesosproto"
+)
+
+type CollectTasksMessage struct {
+	TaskProivders []*lxtypes.TaskProvider `json:"task_providers"`
+}
+
+type UpdateTaskStatusMessage struct {
+	TaskProvider *lxtypes.TaskProvider `json:"task_provider"`
+	TaskStatus *mesosproto.TaskStatus `json:"task_status"`
+}
