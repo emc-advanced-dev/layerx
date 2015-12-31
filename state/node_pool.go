af35896270cef9141808e657dc702a45245dec83
Scott Weiss <scott.weiss@emc.com>
2015-12-31 13:36:00 -0500

    save and load node works

--- state/node_pool.go
+++ state/node_pool.go
@@ -2,6 +2,7 @@ package state
 import (
 	"github.com/layer-x/layerx-commons/lxerrors"
 	"github.com/layer-x/layerx-commons/lxdatabase"
+"github.com/layer-x/layerx-core_v2/lxtypes"
 )
 
 type NodePool struct {
@@ -20,42 +21,90 @@ func (nodePool *NodePool) Initialize() error {
 	return nil
 }
 
-//
-//func (nodePool *NodePool) AddNode(node *lxtypes.Node) error {
-//	nodeId := node.Id
-//	_, err := nodePool.GetNode(nodeId)
-//	if err == nil {
-//		return lxerrors.New("node "+nodeId+" already exists in database, try Modify()?", err)
-//	}
-//	nodeData, err := json.Marshal(node)
-//	if err != nil {
-//		return lxerrors.New("could not marshal node to json", err)
-//	}
-//	err = lxdatabase.Set(nodePool.GetKey()+"/"+nodeId, string(nodeData))
-//	if err != nil {
-//		return lxerrors.New("setting key/value pair for node", err)
-//	}
-//	return nil
-//}
-//
-//func (nodePool *NodePool) GetNode(nodeId string) (*lxtypes.Node, error) {
-//	nodeJson, err := lxdatabase.Get(nodePool.GetKey()+"/"+nodeId)
-//	if err != nil {
-//		return nil, lxerrors.New("retrieving node "+nodeId+" from database", err)
-//	}
-//	var node lxtypes.Node
-//	err = json.Unmarshal([]byte(nodeJson), &node)
-//	if err != nil {
-//		return nil, lxerrors.New("unmarshalling json into Node struct", err)
-//	}
-//	return &node, nil
-//}
-//
-//func (nodePool *NodePool) saveNode(node *lxtypes.Node) error {
-//	nodeId := node.Id
-//	node.Resources
-//	err := lxdatabase.Set(nodePool.GetKey()+"/"+nodeId+"/", string(nodeData))
-//	if err != nil {
-//		return lxerrors.New("setting key/value pair for node", err)
-//	}
-//}
+
+func (nodePool *NodePool) AddNode(node *lxtypes.Node) error {
+	nodeId := node.Id
+	_, err := nodePool.GetNode(nodeId)
+	if err == nil {
+		return lxerrors.New("node "+nodeId+" already exists in database", err)
+	}
+	err = nodePool.saveNode(node)
+	if err != nil {
+		return lxerrors.New("saving node to database", err)
+	}
+	return nil
+}
+
+func (nodePool *NodePool) GetNode(nodeId string) (*lxtypes.Node, error) {
+	_, err := lxdatabase.GetSubdirectories(nodePool.GetKey()+"/"+nodeId)
+	if err != nil {
+		return nil, lxerrors.New("retrieving node "+nodeId+" from database", err)
+	}
+	node, err := nodePool.loadNode(nodeId)
+	if err != nil {
+		return nil, lxerrors.New("loading node "+nodeId+" from stored information in database", err)
+	}
+	return node, nil
+}
+
+func (nodePool *NodePool) saveNode(node *lxtypes.Node) error {
+	nodeId := node.Id
+	err := lxdatabase.Mkdir(nodePool.GetKey()+"/"+nodeId)
+	if err != nil {
+		return lxerrors.New("initializing "+nodePool.GetKey()+"/"+nodeId +" directory", err)
+	}
+	nodeResourcePool := ResourcePool{
+		nodeId: nodeId,
+		rootKey: nodePool.GetKey() + "/" + nodeId + "/resources",
+	}
+	nodeResourcePool.Initialize()
+	for _, resource := range node.Resources {
+		err = nodeResourcePool.AddResource(resource)
+		if err != nil {
+			return lxerrors.New("adding resource "+resource.Id+" to node "+nodeId+" resource pool", err)
+		}
+	}
+	nodeTaskPool := TaskPool{
+		rootKey: nodePool.GetKey() + "/" + nodeId + "/running_tasks",
+	}
+	nodeTaskPool.Initialize()
+	for _, task := range node.RunningTasks {
+		err = nodeTaskPool.AddTask(task)
+		if err != nil {
+			return lxerrors.New("adding task "+task.TaskId+" to node "+nodeId+" task pool", err)
+		}
+	}
+	return nil
+}
+
+func (nodePool *NodePool) loadNode(nodeId string) (*lxtypes.Node, error) {
+	node := lxtypes.NewNode(nodeId)
+	nodeResourcePool := ResourcePool{
+		nodeId: nodeId,
+		rootKey: nodePool.GetKey() + "/" + nodeId + "/resources",
+	}
+	resources, err := nodeResourcePool.GetResources()
+	if err != nil {
+		return nil, lxerrors.New("could not get list of resources for node "+nodeId, err)
+	}
+	for _, resource := range resources {
+		err = node.AddResource(resource)
+		if err != nil {
+			return nil, lxerrors.New("could not add resource to node object", err)
+		}
+	}
+	nodeTaskPool := TaskPool{
+		rootKey: nodePool.GetKey() + "/" + nodeId + "/running_tasks",
+	}
+	tasks, err := nodeTaskPool.GetTasks()
+	if err != nil {
+		return nil, lxerrors.New("could not get list of tasks for node "+nodeId, err)
+	}
+	for _, task := range tasks {
+		err = node.AddTask(task)
+		if err != nil {
+			return nil, lxerrors.New("could not add task to node object", err)
+		}
+	}
+	return node, nil
+}
--- state/node_pool_test.go
+++ state/node_pool_test.go
@@ -1,31 +1,31 @@
 package state_test
-//
-//import (
-//	. "github.com/layer-x/layerx-core_v2/state"
-//
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//	"github.com/layer-x/layerx-core_v2/fakes"
-//)
-//
-//var _ = Describe("NodePool", func() {
-//	Describe("GetNode(nodeId)", func(){
-//		It("returns the node with all of its tasks if it exists, else returns err", func(){
-//			state := NewState()
-//			state.InitializeState("http://127.0.0.1:4001")
-//			PurgeState()
-//			err := state.InitializeState("http://127.0.0.1:4001")
-//			Expect(err).To(BeNil())
-//			nodePool := state.NodePool
-//			fakeNode := fakes.FakeNode("fake_resource_id_1", "fake_node_id_1")
-//			node, err := nodePool.GetNode(fakeNode.Id)
-//			Expect(err).NotTo(BeNil())
-//			Expect(node).To(BeNil())
-//			err = nodePool.AddNode(fakeNode)
-//			Expect(err).To(BeNil())
-//			node, err = nodePool.GetNode(fakeNode.Id)
-//			Expect(err).To(BeNil())
-//			Expect(node).To(Equal(fakeNode))
-//		})
-//	})
-//})
+
+import (
+	. "github.com/layer-x/layerx-core_v2/state"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/layer-x/layerx-core_v2/fakes"
+)
+
+var _ = Describe("NodePool", func() {
+	Describe("GetNode(nodeId)", func(){
+		It("returns the node with all of its tasks if it exists, else returns err", func(){
+			state := NewState()
+			state.InitializeState("http://127.0.0.1:4001")
+			PurgeState()
+			err := state.InitializeState("http://127.0.0.1:4001")
+			Expect(err).To(BeNil())
+			nodePool := state.NodePool
+			fakeNode := fakes.FakeNode("fake_resource_id_1", "fake_node_id_1")
+			node, err := nodePool.GetNode(fakeNode.Id)
+			Expect(err).NotTo(BeNil())
+			Expect(node).To(BeNil())
+			err = nodePool.AddNode(fakeNode)
+			Expect(err).To(BeNil())
+			node, err = nodePool.GetNode(fakeNode.Id)
+			Expect(err).To(BeNil())
+			Expect(node).To(Equal(fakeNode))
+		})
+	})
+})
