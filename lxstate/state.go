265264141a8ff53e7b214e67159df12246ff7128
Scott Weiss <scott.weiss@emc.com>
2016-01-01 14:51:45 -0500

    add tpi and rpi to state

--- lxstate/state.go
+++ lxstate/state.go
@@ -11,6 +11,8 @@ const (
 	staging_tasks = state_root + "/staging_tasks"
 	task_providers = state_root + "/task_providers"
 	statuses = state_root + "/statuses"
+	tpi_url_key = state_root + "/tpi_url"
+	rpi_url_key = state_root + "/rpi_url"
 )
 
 type State struct {
@@ -53,4 +55,36 @@ func (state *State) InitializeState(etcdUrl string) error {
 	state.TaskProviderPool.Initialize()
 	state.StatusPool.Initialize()
 	return nil
+}
+
+func (state *State) SetTpi(tpiUrl string) error {
+	err := lxdatabase.Set(tpi_url_key, tpiUrl)
+	if err != nil {
+		return lxerrors.New("could not set tpi url", err)
+	}
+	return nil
+}
+
+func (state *State) GetTpi() (string, error) {
+	tpiUrl, err := lxdatabase.Get(tpi_url_key)
+	if err != nil {
+		return "", lxerrors.New("could not set tpi url", err)
+	}
+	return tpiUrl, nil
+}
+
+func (state *State) SetRpi(rpiUrl string) error {
+	err := lxdatabase.Set(rpi_url_key, rpiUrl)
+	if err != nil {
+		return lxerrors.New("could not set rpi url", err)
+	}
+	return nil
+}
+
+func (state *State) GetRpi() (string, error) {
+	rpiUrl, err := lxdatabase.Get(rpi_url_key)
+	if err != nil {
+		return "", lxerrors.New("could not set rpi url", err)
+	}
+	return rpiUrl, nil
 }
