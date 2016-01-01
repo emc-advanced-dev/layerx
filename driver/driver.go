778549cf1d3b21ed6cb608c3657898138ab2cda5
Scott Weiss <scott.weiss@emc.com>
2016-01-01 14:29:43 -0500

    add driver

---
+++ driver/driver.go
@@ -0,0 +1,26 @@
+package driver
+import (
+	"github.com/layer-x/layerx-commons/lxactionqueue"
+	"sync"
+)
+
+type coreDriver struct {
+	lock sync.Mutex
+	actionQueue lxactionqueue.ActionQueue
+}
+
+func NewLayerXDriver(actionQueue lxactionqueue.ActionQueue) *coreDriver {
+	return &coreDriver{
+		lock: sync.Mutex{},
+		actionQueue: actionQueue,
+	}
+}
+
+//run as goroutine
+func (d *coreDriver) Run() {
+	for {
+		d.lock.Lock()
+		go d.actionQueue.ExecuteNext()
+		d.lock.Unlock()
+	}
+}
