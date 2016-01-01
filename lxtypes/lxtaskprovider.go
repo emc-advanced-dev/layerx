bdbbef1c841209bead775e188edc969b582f581a
Scott Weiss <scott.weiss@emc.com>
2016-01-01 16:50:34 -0500

    add failover to task provider

--- lxtypes/lxtaskprovider.go
+++ lxtypes/lxtaskprovider.go
@@ -7,4 +7,7 @@ type TaskProvider struct {
 	//this should contain UPID of a framework so
 	//the TPI can send messages to the framework
 	Source string `json:"source"`
+	//indicates whether task provider
+	//has a failover timeout
+	FailoverTimeout float64 `json:"failover_timeout"`
 }
