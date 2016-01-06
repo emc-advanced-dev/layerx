3086ec97994711fe544bf8d984388629174aff3d
Scott Weiss <scott.weiss@emc.com>
2016-01-06 11:04:59 -0500

    task_launcher sweeps staging tasks

---
+++ task_launcher/task_launcher.go
@@ -0,0 +1,72 @@
+package task_launcher
+import (
+	"github.com/layer-x/layerx-core_v2/lxstate"
+	"github.com/layer-x/layerx-commons/lxerrors"
+	"github.com/layer-x/layerx-core_v2/lxtypes"
+	"github.com/layer-x/layerx-core_v2/rpi_messenger"
+	"fmt"
+	"github.com/layer-x/layerx-commons/lxlog"
+	"github.com/Sirupsen/logrus"
+)
+
+type TaskLauncher struct {
+	rpiUrl string
+	state *lxstate.State
+}
+
+func NewTaskLauncher(rpiUrl string, state *lxstate.State) *TaskLauncher {
+	return &TaskLauncher{
+		rpiUrl: rpiUrl,
+		state: state,
+	}
+}
+
+func (tl *TaskLauncher) LaunchStagedTasks() error {
+	stagingTasks, err := tl.state.StagingTaskPool.GetTasks()
+	if err != nil {
+		return lxerrors.New("retrieving staging tasks", err)
+	}
+
+	nodeTaskMap := make(map[string][]*lxtypes.Task)
+	for _, task := range stagingTasks {
+		nodeId := task.SlaveId
+		_, ok := nodeTaskMap[nodeId]
+		if !ok {
+			nodeTaskMap[nodeId] = []*lxtypes.Task{}
+		}
+		nodeTaskMap[nodeId] = append(nodeTaskMap[nodeId], task)
+	}
+	for nodeId, tasksToLaunch := range nodeTaskMap {
+		nodeResourcePool, err := tl.state.NodePool.GetNodeResourcePool(nodeId)
+		if err != nil {
+			return lxerrors.New("finding resource pool for node "+nodeId, err)
+		}
+		resourcesToUseMap, err := nodeResourcePool.GetResources()
+		if err != nil {
+			return lxerrors.New("retrieving resource list for node "+nodeId, err)
+		}
+		resourcesToUse := []*lxtypes.Resource{}
+		for _, resource := range resourcesToUseMap {
+			resourcesToUse = append(resourcesToUse, resource)
+		}
+
+		lxlog.Debugf(logrus.Fields{
+			"tasks": fmt.Sprintf("%v",tasksToLaunch),
+			"resources": fmt.Sprintf("%v", resourcesToUseMap),
+			"node_id": fmt.Sprintf("%s",nodeId),
+			"rpi_url": fmt.Sprintf("%s",tl.rpiUrl),
+		}, "attempting to launch tasks on rpi")
+
+		err = rpi_messenger.SendLaunchTasksMessage(tl.rpiUrl, tasksToLaunch, resourcesToUse)
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{
+				"tasks": fmt.Sprintf("%v",tasksToLaunch),
+				"resources": fmt.Sprintf("%v", resourcesToUseMap),
+				"node_id": fmt.Sprintf("%s",nodeId),
+				"rpi_url": fmt.Sprintf("%s",tl.rpiUrl),
+			}, "trying to launch tasks on rpi")
+			return lxerrors.New("sending launch task message to rpi", err)
+		}
+	}
+	return nil
+}
---
+++ task_launcher/task_launcher_test.go
@@ -0,0 +1,122 @@
+package task_launcher_test
+
+import (
+	. "github.com/layer-x/layerx-core_v2/task_launcher"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/layer-x/layerx-core_v2/layerx_rpi_client"
+	"github.com/layer-x/layerx-core_v2/layerx_tpi_client"
+	"github.com/layer-x/layerx-core_v2/layerx_brain_client"
+	"github.com/layer-x/layerx-core_v2/lxstate"
+	"github.com/layer-x/layerx-commons/lxactionqueue"
+"github.com/layer-x/layerx-core_v2/driver"
+	"github.com/layer-x/layerx-commons/lxmartini"
+	"fmt"
+"github.com/layer-x/layerx-core_v2/fakes"
+"github.com/layer-x/layerx-commons/lxlog"
+	"github.com/layer-x/layerx-core_v2/lxserver"
+	"github.com/layer-x/layerx-commons/lxdatabase"
+"github.com/layer-x/layerx-core_v2/lxtypes"
+)
+
+func PurgeState() {
+	lxdatabase.Rmdir("/state", true)
+}
+
+var _ = Describe("TaskLauncher", func() {
+
+	var lxRpiClient *layerx_rpi_client.LayerXRpi
+	var lxTpiClient *layerx_tpi_client.LayerXTpi
+	var lxBrainClient *layerx_brain_client.LayerXBrainClient
+	var state *lxstate.State
+	var serverErr error
+
+	Describe("setup", func() {
+		It("sets up for the tests", func() {
+			lxRpiClient = &layerx_rpi_client.LayerXRpi{
+				CoreURL: "127.0.0.1:2277",
+			}
+			lxTpiClient = &layerx_tpi_client.LayerXTpi{
+				CoreURL: "127.0.0.1:2277",
+			}
+			lxBrainClient = &layerx_brain_client.LayerXBrainClient{
+				CoreURL: "127.0.0.1:2277",
+			}
+
+			actionQueue := lxactionqueue.NewActionQueue()
+			state = lxstate.NewState()
+			err := state.InitializeState("http://127.0.0.1:4001")
+			Expect(err).To(BeNil())
+			coreServerWrapper := lxserver.NewLayerXCoreServerWrapper(state, actionQueue)
+			driver := driver.NewLayerXDriver(actionQueue)
+
+			driverErrc := make(chan error)
+			go func() {
+				for {
+					serverErr = <-driverErrc
+				}
+			}()
+
+			m := coreServerWrapper.WrapServer(lxmartini.QuietMartini(), "127.0.0.1:2288", "127.0.0.1:2299", driverErrc)
+			go m.RunOnAddr(fmt.Sprintf(":2277"))
+			go fakes.RunFakeTpiServer("127.0.0.1:2277", 2288, make(chan error))
+			go fakes.RunFakeRpiServer("127.0.0.1:2277", 2299, make(chan error))
+			go driver.Run()
+			lxlog.ActiveDebugMode()
+		})
+	})
+	Describe("LaunchStagedTasks", func(){
+		It("sends LaunchTaskMessage to rpi for all tasks in the staging pool", func(){
+			PurgeState()
+			err2 := state.InitializeState("http://127.0.0.1:4001")
+			Expect(err2).To(BeNil())
+			fakeResource1 := lxtypes.NewResourceFromMesos(fakes.FakeOffer("fake_offer_id_1", "fake_slave_id_1"))
+			fakeNode1 := lxtypes.NewNode(fakeResource1.NodeId)
+			err := fakeNode1.AddResource(fakeResource1)
+			Expect(err).To(BeNil())
+			err = state.NodePool.AddNode(fakeNode1)
+			fakeTask1 := fakes.FakeLXTask("fake_task_id_1", "fake_task_name", "fake_slave_id", "echo FAKE_COMMAND")
+			fakeTaskProvider := &lxtypes.TaskProvider{
+				Id:     "fake_task_provider_id_1",
+				Source: "taskprovider1@tphost:port",
+			}
+			fakeTask1.SlaveId = fakeNode1.Id
+			fakeTask1.TaskProvider = fakeTaskProvider
+			fakeTask2 := fakes.FakeLXTask("fake_task_id_2", "fake_task_name", "fake_slave_id", "echo FAKE_COMMAND")
+			fakeTask2.SlaveId = fakeNode1.Id
+			fakeTask2.TaskProvider = fakeTaskProvider
+			fakeTask3 := fakes.FakeLXTask("fake_task_id_3", "fake_task_name", "fake_slave_id", "echo FAKE_COMMAND")
+			fakeTask3.SlaveId = fakeNode1.Id
+			fakeTask3.TaskProvider = fakeTaskProvider
+
+			err = state.StagingTaskPool.AddTask(fakeTask1)
+			Expect(err).To(BeNil())
+
+			err = state.StagingTaskPool.AddTask(fakeTask2)
+			Expect(err).To(BeNil())
+
+			err = state.StagingTaskPool.AddTask(fakeTask3)
+			Expect(err).To(BeNil())
+
+			taskLauncher := NewTaskLauncher("127.0.0.1:2299", state)
+			err = taskLauncher.LaunchStagedTasks()
+			Expect(err).To(BeNil())
+
+			stagingTasks, err := state.StagingTaskPool.GetTasks()
+			Expect(err).To(BeNil())
+			Expect(stagingTasks).To(BeEmpty())
+			node1TaskPool, err := state.NodePool.GetNodeTaskPool(fakeNode1.Id)
+			Expect(err).To(BeNil())
+			node1Task1, err := node1TaskPool.GetTask(fakeTask1.TaskId)
+			Expect(err).To(BeNil())
+			Expect(node1Task1).To(Equal(fakeTask1))
+			node1Task2, err := node1TaskPool.GetTask(fakeTask2.TaskId)
+			Expect(node1Task2).To(Equal(fakeTask2))
+			Expect(err).To(BeNil())
+			node1Task3, err := node1TaskPool.GetTask(fakeTask3.TaskId)
+			Expect(node1Task3).To(Equal(fakeTask3))
+			Expect(err).To(BeNil())
+		})
+	})
+})
