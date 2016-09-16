bfd13bed31525a9ca4a9d5d62f0f9be4c1193c16
Scott Weiss <scott.weiss@emc.com>
2016-09-16 17:45:20 -0400

    fix compile errors
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- layerx-k8s-rpi/server/server.go
+++ layerx-k8s-rpi/server/server.go
@@ -22,19 +22,23 @@ func Start(port string, client *kube.Client, core *layerx_rpi_client.LayerXRpi) 
 	m := lxmartini.QuietMartini()
 
 	m.Post(COLLECT_RESOURCES, func(req *http.Request, res http.ResponseWriter) {
-		handle(res, func() ([]byte, int, error) {
-			nodes, err := client.FetchResources()
+		handle(res, func() (interface{}, int, error) {
+			resources, err := client.FetchResources()
 			if err != nil {
 				return nil, 500, errors.New("could not handle collect resources request", err)
 			}
-			go func(){
-				core.SubmitResource()
-			}()
+			for _, resource := range resources {
+				go func(){
+					if err := core.SubmitResource(resource); err != nil {
+						logrus.WithError(err).Errorf("failed submitting resource %v to core %v", resource, core)
+					}
+				}()
+			}
 			return nil, 202, nil
 		})
 	})
 	m.Post(LAUNCH_TASKS, func(req *http.Request, res http.ResponseWriter) {
-		handle(res, func() ([]byte, int, error) {
+		handle(res, func() (interface{}, int, error) {
 			data, err := ioutil.ReadAll(req.Body)
 			if req.Body != nil {
 				defer req.Body.Close()
@@ -56,7 +60,7 @@ func Start(port string, client *kube.Client, core *layerx_rpi_client.LayerXRpi) 
 		})
 	})
 	m.Post(KILL_TASK + "/:task_id", func(req *http.Request, res http.ResponseWriter, params martini.Params) {
-		handle(res, func() ([]byte, int, error) {
+		handle(res, func() (interface{}, int, error) {
 			taskId := params["task_id"]
 			if err := client.KillTask(taskId); err != nil {
 				logrus.WithFields(logrus.Fields{
@@ -68,7 +72,7 @@ func Start(port string, client *kube.Client, core *layerx_rpi_client.LayerXRpi) 
 		})
 	})
 
-	m.RunOnAddr(":" + *port)
+	m.RunOnAddr(":" + port)
 }
 
 func handle(res http.ResponseWriter, action func() (interface{}, int, error)) {
