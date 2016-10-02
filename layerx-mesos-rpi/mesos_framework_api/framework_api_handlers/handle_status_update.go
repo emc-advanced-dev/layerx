0dcb2a85316e74dbab9bcbd967d17fa479b053c2
Scott Weiss <scott.weiss@emc.com>
2016-10-02 11:12:26 -0400

    hacky workaround for duplicate id bug on mesos
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- layerx-mesos-rpi/mesos_framework_api/framework_api_handlers/handle_status_update.go
+++ layerx-mesos-rpi/mesos_framework_api/framework_api_handlers/handle_status_update.go
@@ -4,10 +4,17 @@ import (
 	"github.com/emc-advanced-dev/layerx/layerx-core/layerx_rpi_client"
 	"github.com/emc-advanced-dev/pkg/errors"
 	"github.com/mesos/mesos-go/mesosproto"
+	"strings"
 )
 
 func HandleStatusUpdate(lxRpi *layerx_rpi_client.LayerXRpi, status *mesosproto.TaskStatus) error {
-	statusString := "task " + status.GetTaskId().GetValue() + " in state" + status.GetState().String()
+	taskState := status.GetState().String()
+	statusString := "task " + status.GetTaskId().GetValue() + " in state" + taskState
+	//ignore duplicate id bug
+	//TODO: don't ignore this bug, but figure out where it's coming from
+	if strings.Contains(taskState, "duplicate") {
+		return nil
+	}
 	err := lxRpi.SubmitStatusUpdate(status)
 	if err != nil {
 		return errors.New("failed to submit status {"+statusString+"} to layerx core", err)
