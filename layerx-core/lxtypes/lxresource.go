5625a9dbcd0177c2ef51eed190e6c0211d053d3e
Scott Weiss <scott.weiss@emc.com>
2016-10-03 14:27:50 -0400

    add rpi type for slave; add a way to diff port ranges
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- layerx-core/lxtypes/lxresource.go
+++ layerx-core/lxtypes/lxresource.go
@@ -1,10 +1,125 @@
 package lxtypes
 
-import "github.com/mesos/mesos-go/mesosproto"
+import (
+	"github.com/mesos/mesos-go/mesosproto"
+	"sort"
+)
 
 type PortRange struct {
 	Begin uint64 `json:"begin"`
 	End   uint64 `json:"end"`
+}
+
+type rangesSorter []PortRange
+
+func (s rangesSorter) Len() int {
+	return len(s)
+}
+func (s rangesSorter) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s rangesSorter) Less(i, j int) bool {
+	return s[i].Begin < s[j].Begin
+}
+
+func sortRanges(prlist []PortRange) {
+	sort.Sort(rangesSorter(prlist))
+}
+
+type stack []PortRange
+
+func (s stack) Push(v PortRange) stack {
+	return append(s, v)
+}
+
+func (s stack) Pop() (stack, PortRange) {
+	if len(s) < 1 {
+		return PortRange{}
+	}
+
+	l := len(s)
+	return  s[:l-1], s[l-1]
+}
+
+func (pr1 PortRange) subtractRange(pr2 PortRange) []PortRange {
+	//nothing to remove
+	if pr1.Begin > pr2.End && pr1.End > pr2.Begin {
+		return []PortRange{pr1}
+	}
+	res := []PortRange{}
+	//overlaps beginning of pr2
+	if pr1.Begin < pr2.Begin && pr1.End > pr2.Begin {
+		res = append(res, PortRange{
+			Begin: pr1.Begin,
+			End: pr2.Begin - 1,
+		})
+	}
+	//overlaps end of pr2
+	if pr1.Begin < pr2.Begin && pr1.End > pr2.Begin {
+		res = append(res, PortRange{
+			Begin: pr2.End + 1,
+			End: pr1.End,
+		})
+	}
+	return res
+}
+
+func (pr1 PortRange) overlaps(pr2 PortRange) bool {
+	return (pr1.Begin < pr2.Begin && pr1.End > pr2.Begin) || (pr1.Begin < pr2.End && pr1.End > pr2.End)
+}
+
+func (pr1 PortRange) overlapsAny(prList []PortRange) bool {
+	for _, pr2 := range prList {
+		if pr1.overlaps(pr2) {
+			return true
+		}
+	}
+	return false
+}
+
+func overlaps(prList1, prList2 []PortRange) bool {
+	for _, pr1 := range prList1 {
+		if pr1.overlapsAny(prList2) {
+			return true
+		}
+	}
+	return false
+}
+
+//returns PRList1 minus intersections with PRList2
+func DiffPortRanges(rl1, rl2 []PortRange) []PortRange {
+	sortRanges(rl1)
+	sortRanges(rl2)
+	if !overlaps(rl1, rl2) {
+		return rl1
+	}
+	results := []PortRange{}
+	addResult := func(result PortRange) {
+		added := false
+		for _, pr := range results {
+			if pr == result {
+				added = true
+				break
+			}
+		}
+		if !added{
+			results = append(results, result)
+		}
+	}
+	for i, i1 := range rl1 {
+		if !i1.overlapsAny(rl2) {
+			addResult(i1)
+		} else {
+			//find overlap
+			for _, i2 := range rl2 {
+				if i1.overlaps(i2) {
+					diffs := i1.subtractRange(i2)
+					return DiffPortRanges(append(append(rl1[:i], diffs...), rl1[i+1:]...), rl2)
+				}
+			}
+		}
+	}
+	return results
 }
 
 type ResourceType string
---
+++ layerx-mesos-rpi/types/slave.go
@@ -0,0 +1,109 @@
+package types
+
+import (
+	"github.com/emc-advanced-dev/layerx/layerx-core/lxtypes"
+	"strings"
+	"strconv"
+	"github.com/emc-advanced-dev/pkg/errors"
+)
+
+type Slaves struct {
+	Slaves []Slave `json:"slaves"`
+}
+
+type Slave struct {
+	ID                    string `json:"id"`
+	Pid                   string `json:"pid"`
+	Hostname              string `json:"hostname"`
+	RegisteredTime        float64 `json:"registered_time"`
+	ReregisteredTime      float64 `json:"reregistered_time"`
+	Resources             struct {
+				      Cpus  float64 `json:"cpus"`
+				      Disk  float64 `json:"disk"`
+				      Mem   float64 `json:"mem"`
+				      Ports string `json:"ports"`
+			      } `json:"resources"`
+	UsedResources         struct {
+				      Cpus  float64 `json:"cpus"`
+				      Disk  float64 `json:"disk"`
+				      Mem   float64 `json:"mem"`
+				      Ports string `json:"ports"`
+			      } `json:"used_resources"`
+	OfferedResources      struct {
+				      Cpus float64 `json:"cpus"`
+				      Disk float64 `json:"disk"`
+				      Mem  float64 `json:"mem"`
+			      } `json:"offered_resources"`
+	ReservedResources     struct {
+			      } `json:"reserved_resources"`
+	UnreservedResources   struct {
+				      Cpus  float64 `json:"cpus"`
+				      Disk  float64 `json:"disk"`
+				      Mem   float64 `json:"mem"`
+				      Ports string `json:"ports"`
+			      } `json:"unreserved_resources"`
+	Attributes            struct {
+			      } `json:"attributes"`
+	Active                bool `json:"active"`
+	Version               string `json:"version"`
+	ReservedResourcesFull struct {
+			      } `json:"reserved_resources_full"`
+	UsedResourcesFull     []struct {
+		Name   string `json:"name"`
+		Type   string `json:"type"`
+		Scalar struct {
+			       Value float64 `json:"value"`
+		       } `json:"scalar,omitempty"`
+		Role   string `json:"role"`
+		Ranges struct {
+			       Range []struct {
+				       Begin int `json:"begin"`
+				       End   int `json:"end"`
+			       } `json:"range"`
+		       } `json:"ranges,omitempty"`
+	} `json:"used_resources_full"`
+	OfferedResourcesFull  []interface{} `json:"offered_resources_full"`
+}
+
+func (s *Slave) ToResource() (*lxtypes.Resource, error) {
+	totalPorts, err := parsePortsString(s.Resources.Ports)
+	if err != nil {
+		return nil, errors.New("failed to parse ports string "+s.Resources.Ports, err)
+	}
+	usedPorts, err := parsePortsString(s.UsedResources.Ports)
+	if err != nil {
+		return nil, errors.New("failed to parse ports string "+s.UsedResources.Ports, err)
+	}
+	return &lxtypes.Resource{
+		Id:     s.ID,
+		NodeId: s.ID,
+		Cpus:   s.Resources.Cpus - s.UsedResources.Cpus,
+		Mem:    s.Resources.Mem - s.UsedResources.Mem,
+		Disk:   s.Resources.Disk - s.UsedResources.Disk,
+		Ports:  lxtypes.DiffPortRanges(totalPorts, usedPorts),
+		ResourceType: lxtypes.ResourceType_Mesos,
+	}, nil
+}
+
+func parsePortsString(portString string) ([]lxtypes.PortRange, error) {
+	ports := []lxtypes.PortRange{}
+	portString = strings.TrimPrefix(portString, "[")
+	portString = strings.TrimSuffix(portString, "]")
+	portRanges := strings.Split(portString, ", ")
+	for _, portRange := range portRanges {
+		split := strings.Split(portRange, "-")
+		begin, err := strconv.Atoi(split[0])
+		if err != nil {
+			return nil, errors.New("could not parse "+split[0]+" as integer", err)
+		}
+		end, err := strconv.Atoi(split[1])
+		if err != nil {
+			return nil, errors.New("could not parse "+split[1]+" as integer", err)
+		}
+		ports = append(ports, lxtypes.PortRange{
+			Begin: uint64(begin),
+			End: uint64(end),
+		})
+	}
+	return ports, nil
+}
