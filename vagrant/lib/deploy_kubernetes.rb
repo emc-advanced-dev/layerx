20422a042f02f4be908f142b09ab3410fe7b49a9
Scott Weiss <scott.weiss@emc.com>
2016-11-09 12:23:36 -0500

    vagrant status and stuff appear to work for minkube
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- vagrant/lib/deploy_kubernetes.rb
+++ vagrant/lib/deploy_kubernetes.rb
@@ -1,5 +1,6 @@
-require './helpers'
+require_relative './helpers'
 require 'yaml'
+
 
 def get_kubeconfig
   kubeconfig = "#{ENV['HOME']}/.kube/config"
@@ -8,30 +9,41 @@ def get_kubeconfig
 end
 
 def deploy_kubernetes conf
+  puts "deploying kubernetes"
   system("minikube start --host-only-cidr=\"#{conf['k8s_ip_base']}1/24\"")
   get_kubeconfig
 end
 
 def stop_kubernetes
+  puts "stopping kubernetes"
   system("minikube stop")
 end
 
 def destroy_kubernetes
+  unless ARGV.include? "-f"
+    puts "Are you sure you want to destroy the 'minikube' VM? [y/N]"
+    ans = gets
+    unless ans == 'y'
+      return
+    end
+  end
+  puts "destroy kubernetes"
   system("minikube delete")
 end
 
 def reload_kubernetes conf
+  puts "reloading kubernetes"
   destroy_kubernetes
   deploy_kubernetes conf
 end
 
+def status_kubernetes
+  system("minikube status")
+end
+
 def handle_kubernetes_action conf
   unless in_path?("minikube")
-    puts "please install kubectl before deploying kubernetes"
-    exit(1)
-  end
-  unless in_path?("minikube")
-    puts "please install minikube before deploying kubernetes"
+    puts "please install minikube before deploying kubernetes (make sure it is in PATH)"
     exit(1)
   end
 
@@ -44,5 +56,7 @@ def handle_kubernetes_action conf
       reload_kubernetes conf
     when "destroy"
       destroy_kubernetes
+    when "status"
+      status_kubernetes
   end
 end
--- vagrant/lib/helpers.rb
+++ vagrant/lib/helpers.rb
@@ -13,7 +13,7 @@ def in_path?(cmd)
   ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
     exts.each { |ext|
       exe = File.join(path, "#{cmd}#{ext}")
-      exe if File.executable?(exe) && !File.directory?(exe)
+      return exe if File.executable?(exe) && !File.directory?(exe)
     }
   end
   nil
